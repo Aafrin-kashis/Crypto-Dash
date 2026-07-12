ad19f3d6dde8704d45263313096d9f281b938a7a
Aafrin-kashis <sunderc178@gmail.com>
2026-07-12 15:08:59 -0400

    In API file aading one new function fetchUSDToINR(), for fetching Coin data

--- js/api.js
+++ js/api.js
@@ -1,4 +1,6 @@
 const API_URL = "https://api.coingecko.com/api/v3";
+const CURRENCY_API = 
+"https://api.exchangerate-api.com/v4/latest/USD";
 
 // Fetch coin data
 export async function fetchCoin(coin = "bitcoin") {
@@ -12,4 +14,16 @@ export async function fetchHistory(coin = "bitcoin") {
   const res = await fetch(`${API_URL}/coins/${coin}/market_chart?vs_currency=usd&days=7`);
   if (!res.ok) throw new Error("Failed to fetch history");
   return res.json();
+}
+
+export async function fetchUSDToINR(){
+
+  const res = await fetch(CURRENCY_API);
+
+  if(!res.ok){
+    throw new Error("Currency fetch failed");
+  }
+
+  return res.json();
+
 }
